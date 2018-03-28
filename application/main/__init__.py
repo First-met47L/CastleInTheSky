b44e76db2158d8871ed3578f11caf5a08ae7fb63
firstmet <351264614@qq.com>
2018-03-28 12:02:44 +0800

    [update] email

--- application/main/__init__.py
+++ application/main/__init__.py
@@ -1,5 +1,17 @@
 from flask import Blueprint
+from ..models import Permission
 
 main = Blueprint ('main', __name__)
 
 from . import errors, forms, views
+
+
+@main.app_context_processor
+def inject_permissions():
+    '''
+    在模板中可能也需要检查权限,所以 Permission 类为所有位定义了常量以便于获取。为了
+    避免每次调用 render_template() 时都多添加一个模板参数,可以使用上下文处理器。上
+    下文处理器能让变量在所有模板中全局可访问
+    :return: a dict object
+    '''
+    return dict (Permission=Permission)
--- application/main/views.py
+++ application/main/views.py
@@ -1,33 +1,42 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for
-from flask_login import login_required,current_user
+from flask_login import login_required, current_user
+from ..decorator import admin_required
 from . import main
 from .forms import NameForm
 from .. import db
 from ..models import User
 
 
-
 @main.route ('/index', methods=['GET', 'POST'])
 def index():
     form = NameForm ()
     if form.validate_on_submit ():
-        user = User.query.filter(User.username == form.name.data).first()
+        user = User.query.filter (User.username == form.name.data).first ()
         if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
+            user = User (username=form.name.data)
+            db.session.add (user)
+            db.session.commit ()
             session['known'] = False
         else:
             session['known'] = True
         session['name'] = form.name.data
         # form.name.data = ''
-        return redirect (url_for ('.index')) #abbreviation of main.submit_page
+        return redirect (url_for ('.index'))  # abbreviation of main.submit_page
         # return redirect (url_for ('main.index'))
 
-    return render_template ('index.html', form=form, name=session.get ('name'), known = session.get('known', False),current_user=current_user)
+    return render_template ('index.html', form=form, name=session.get ('name'), known=session.get ('known', False),
+                            current_user=current_user)
 
-@main.route('/secret')
+
+@main.route ('/secret')
 @login_required
 def secret():
     return 'Only authenticated user are allowed'
+
+
+@main.route ('/admin')
+@login_required
+@admin_required
+def for_admins_only():
+    return "For administrators!"
